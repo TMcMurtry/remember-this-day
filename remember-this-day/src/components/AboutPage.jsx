344f531f0dc30de2791646383773716b945eac08
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-06 10:09:43 -0600

    Added base aboutpage content, entry submission form

--- remember-this-day/src/components/AboutPage.jsx
+++ remember-this-day/src/components/AboutPage.jsx
@@ -1,7 +1,11 @@
 export default function AboutPage(){
 
     return(
-
+        <div>
+            <h1>About</h1>
+            <p>This is where I am going to put all the information 
+                about the project and how to get the most of it. </p>
+        </div>
     )
 }
 
--- remember-this-day/src/components/EntrySubmission.jsx
+++ remember-this-day/src/components/EntrySubmission.jsx
@@ -5,11 +5,11 @@ export default function EntrySubmission(){
             <label for="title">
                 <input name="title" id="title" type="text"  placeholder="Enter Title">Title</input>
             </label>
-            <label>
-                <input></input>
+            <label for="entrytext">
+                <textarea name="entrytext" id="entrytext" placeholder="Type your entry here" cols={30} rows={20} required>Entry Text</textarea>
             </label>
-            <label>
-                <input></input>
+            <label for="dateinput">
+                <input name="dateinput" id="dateinput" type="date" required>Date</input>
             </label>
         </form>
     )
