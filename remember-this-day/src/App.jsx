7ac269362a44ebb5edc5f4e81d062b9f25845f12
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-06 12:45:30 -0600

    Added initial navigation with Routes, Route, and a link

--- remember-this-day/src/App.jsx
+++ remember-this-day/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router';
 import Login from './components/Login'
 import EntrySubmission from './components/MainEntryPage/EntrySubmission'
 import AboutPage from './components/AboutPage'
@@ -7,9 +8,15 @@ function App() {
 
   return (
     <div>
-      <Login/>
-      <AboutPage/>
-      <EntrySubmission/>
+      <h1>Remember This Day</h1>
+      <nav>
+        <Link to="/about">About this application</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/entries" element={<EntrySubmission/>}/>
+      </Routes>
     </div>
   )
 }
--- remember-this-day/src/components/MainEntryPage/EntrySubmission.jsx
+++ remember-this-day/src/components/MainEntryPage/EntrySubmission.jsx
@@ -1,1 +1,11 @@
 import EntrySubmissionForm from './EntrySubmissionForm'
+
+export default function EntrySubmission(){
+
+    return(
+        <div>
+            <h1>Hi</h1>
+            <EntrySubmissionForm/>
+        </div>
+    )
+}
