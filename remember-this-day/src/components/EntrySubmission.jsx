1be6390f9e3b8ce6cb31efa3938b6da0a1dc535e
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-06 09:51:30 -0600

    Login submission form added

--- remember-this-day/src/components/EntrySubmission.jsx
+++ remember-this-day/src/components/EntrySubmission.jsx
@@ -1,6 +1,16 @@
 export default function EntrySubmission(){
 
     return(
-
+        <form>
+            <label for="title">
+                <input name="title" id="title" type="text"  placeholder="Enter Title">Title</input>
+            </label>
+            <label>
+                <input></input>
+            </label>
+            <label>
+                <input></input>
+            </label>
+        </form>
     )
 }
--- remember-this-day/src/components/Login.jsx
+++ remember-this-day/src/components/Login.jsx
@@ -2,13 +2,13 @@ export default function Login(){
 
     return(
         <form>
-            <label for='username'>
+            <label for="username">
                 <input id="username" type="text" name="username" placeholder="Username" required>Username</input>
             </label>
-            <label for='password'>
+            <label for="password">
                 <input id="password" name="password" type="password" placeholder="Password" required>Password</input>
             </label>
-            <button>Log In</button>
+            <button name="login" id="login" type="submit">Log In</button>
         </form>
     )
 }
