a40e0281bdefdd5f9156ca01b6284032315aa133
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-07 13:58:27 -0600

    Finished initial login logic, created separate component for header and footer

--- remember-this-day/src/components/Login.jsx
+++ remember-this-day/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import UserData from '../UserData.json'
 export default function Login({isLoggedIn, setIsLoggedIn}){
-    //user inputs data
  //on click, username input is checked against current profiles
  //if username is matched, checks password of that user profile
  //if either don't match it throws error 
@@ -10,8 +9,24 @@ export default function Login({isLoggedIn, setIsLoggedIn}){
  //pressing the log out page logs out
     const [usernameInput, setUsernameInput] = useState("");
     const [passwordInput, setPasswordInput] = useState("");
-    const handleUsernameChange = (ev) => setUsernameInput(ev.target.value)
-    const handlePasswordChange = (ev) => setPasswordInput(ev.target.value)
+    const handleUsernameChange = (ev) => setUsernameInput(ev.target.value);
+    const handlePasswordChange = (ev) => setPasswordInput(ev.target.value);
+    const trimmedUsernameInput = usernameInput.trim();
+    const trimmedPasswordInput = passwordInput.trim();
+    let failedLoginMessage = null;
+    
+    
+    function passwordVerify(){
+        let passwordToMatch = null;
+        const findUsername = UserData.userProfiles.filter((user) => 
+        (user.username === trimmedUsernameInput ))
+       if (findUsername.length === 0){
+        return("Incorrect username or password")
+       } else {
+         passwordToMatch = findUsername[0].password;
+       }
+       passwordToMatch === trimmedPasswordInput ? setIsLoggedIn = true : failedLoginMessage = "Incorrect username or password";
+    }
 
     return(
         <div>
@@ -24,9 +39,9 @@ export default function Login({isLoggedIn, setIsLoggedIn}){
                 <input id="password" name="password" type="password" 
                 value={passwordInput} onChange={handlePasswordChange} placeholder="Password" required/>
             </label>
-            <button name="login" id="login" type="submit" >Log In</button>
+            <button name="login" id="login" type="submit" onClick={passwordVerify}>Log In</button>
         </form>
-        <p>{usernameInput}</p>
+        <p>{trimmedUsernameInput}</p>
         <p>{passwordInput}</p>
         </div>
     )
