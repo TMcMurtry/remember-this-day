45c92653304d9b07b0d329b4b1b43c47e2fbb39c
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-10 09:13:57 -0600

    Finished functionality for dynamic submission form

--- remember-this-day/src/components/MainEntryPage/EntrySubmissionForm.jsx
+++ remember-this-day/src/components/MainEntryPage/EntrySubmissionForm.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react"
+import UserData from '../../UserData.json'
 
 export default function EntrySubmissionForm({currentUser, setCurrentUser}){
-    const [entryTitle, setEntryTitle] = useState();
-    const [entryTextBody, setEntryTextBody] = useState();
-    const [entryDate, setEntryDate] = useState();
+    const [entryTitle, setEntryTitle] = useState("");
+    const [entryTextBody, setEntryTextBody] = useState("");
+    const [entryDate, setEntryDate] = useState("");
     const handleTitleChange = (ev) => setEntryTitle(ev.target.value);
     const handleEntryTextChange = (ev) => setEntryTextBody(ev.target.value);
     const handleDateChange = (ev) => setEntryDate(ev.target.value);
-    function entryAppend(){
-        setCurrentUser(
-            ...currentUser
-        )
-    }
+
+    function handleEntryAppend(ev){
+        ev.preventDefault();
+        const newEntry = { title: entryTitle, date: entryDate, entry: entryTextBody };
+        setCurrentUser({...currentUser, entries:[...currentUser.entries, newEntry]});
+        setEntryTitle("");
+        setEntryTextBody("");
+        setEntryDate("");
+    };
+
+    //Create an object using the data
+    //take id of user
+    //find matching id of userData
+    //append data to json file
+    //increment numberofEntries count
 
     return(
-        <form>
+        <form onSubmit={handleEntryAppend}>
             <label htmlFor="title">
                 <input name="title" id="title" type="text" value={entryTitle} onChange={handleTitleChange} placeholder="Enter Title"/>
             </label>
@@ -24,7 +35,7 @@ export default function EntrySubmissionForm({currentUser, setCurrentUser}){
             <label htmlFor="dateInput">
                 <input name="dateInput" id="dateInput" type="date" value={entryDate} onChange={handleDateChange} required/>
             </label>
-            <button id="entrySubmitButton" type="submit" onClick={entryAppend}>Submit Entry</button>
+            <button id="entrySubmitButton" type="submit" >Submit Entry</button>
         </form>
     )
 }
