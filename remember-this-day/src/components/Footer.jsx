157b56d09001f5c6b498687314be37ecb80e71c6
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-15 08:31:08 -0600

    Reformatted and modified footer content

--- remember-this-day/src/components/Footer.jsx
+++ remember-this-day/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import './Footer.css'
 
 export default function Footer(){
+///Timothy McMurtry 
 
     return(
         <div className='footerDisplay'>
-            <p>Made by Timothy McMurtry</p>
+            <p>Made by Timothy McMurtry  -- Connect with me via: </p>
+                        <a href='https://www.linkedin.com/in/timothy-mcmurtry/'>LinkedIn</a>
         </div>
     )
 };
