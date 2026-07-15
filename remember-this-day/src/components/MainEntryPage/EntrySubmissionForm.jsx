a624d69c59771457aa45dc4e7f285fb5ecb0f459
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-15 08:09:24 -0600

    Finished item placement for submission page styling

--- remember-this-day/src/components/MainEntryPage/EntrySubmissionForm.jsx
+++ remember-this-day/src/components/MainEntryPage/EntrySubmissionForm.jsx
@@ -22,13 +22,14 @@ export default function EntrySubmissionForm({currentUser, setCurrentUser, backgr
     return(
         <form className="entrySubmissionForm" onSubmit={handleEntryAppend}>
             <h2>Submit a new journal entry!</h2>
-            <label htmlFor="title">Title:
+            <label htmlFor="title">Title: <br/>
                 <input name="title" id="title" type="text" value={entryTitle} onChange={handleTitleChange} placeholder="Enter Title"/>
             </label>
             <label htmlFor="entryText">
-                <textarea name="entryText" id="entryText" value={entryTextBody} onChange={handleEntryTextChange} placeholder="Type your entry here" required/>
+                <textarea name="entryText" id="entryText" value={entryTextBody} onChange={handleEntryTextChange} 
+                cols={40} rows={6} placeholder="Type your entry here" required/>
             </label>
-            <label htmlFor="dateInput">
+            <label htmlFor="dateInput">Date: <br/>
                 <input name="dateInput" id="dateInput" type="date" value={entryDate} onChange={handleDateChange} required/>
             </label>
             <button id="entrySubmitButton" type="submit" >Submit Entry</button>
--- remember-this-day/src/components/MainEntryPage/Prompts/ViewPrompts.jsx
+++ remember-this-day/src/components/MainEntryPage/Prompts/ViewPrompts.jsx
@@ -15,10 +15,11 @@ export default function ViewPrompts(){
     }
 
     return(
-        <div className='promptDisplay'>
+        <div className='promptDisplayArea'>
             { promptDisplay && <div className='promptDisplay'><h2>Prompt Category: {promptDisplay.category}</h2>
             <ul>
-                {promptDisplay.prompts.map((prompt, index) => <li key={index} >{prompt}</li>)}
+                {promptDisplay.prompts.map((prompt, index) =>
+                     <li key={index} >{prompt}</li>)}
             </ul>
             </div>}
             <label htmlFor="viewPrompts">
