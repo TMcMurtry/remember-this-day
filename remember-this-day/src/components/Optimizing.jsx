5b8a342c453d0e85212d6715ed2c3a7b2500179d
Timothy M <mr.timmcmurtry@gmail.com>
2026-07-20 09:46:15 -0600

    Modified vite config settings for deployment, changed file paths for images to avoid 404 error

--- remember-this-day/src/components/Optimizing.jsx
+++ remember-this-day/src/components/Optimizing.jsx
@@ -5,14 +5,14 @@ export default function Optimizing(){
     return(
         <div className='optimizing'>
             <h1>About</h1>
-            <img src='/images/Person-journaling.jpg' alt='Image of a person journaling'id='journalingImage'></img>
+            <img src='./images/Person-journaling.jpg' alt='Image of a person journaling'id='journalingImage'></img>
             <p id='p1'>The point of this application is to keep positivity in the forefront of your mind! 
                 So the first step in this process is to use the journal entry submission form to write about good things that happen to you throughout the day
                 or good things that you remember previously happening to you! Do this as often as possible because you will want lots of entries for the next part.
                  Remember, this specific journal is meant for good things only, and if you are having a hard time thinking of good things to write about, 
                  you can push the button in the entry page that says "View Writing Prompts". It helps to be specific about what made you happy and why, because you will be looking back on this entry. 
                 </p>
-            <img src='/images/Person-reading.jpg' alt='Image of a person reading' id='readingImage'></img>
+            <img src='./images/Person-reading.jpg' alt='Image of a person reading' id='readingImage'></img>
             <p id='p2'>The second step in this process is to remember the good times! Whether you are having a rough day and need a little pick me up,
                 or if you just want to relive a happy memory for a moment, press the "View Past Entry" in the entry page, and a previously submitted entry will appear. 
                 Take a minute to really relive the memory and see if it can't put you in a little bit of a happier mood.  </p>
@@ -22,7 +22,7 @@ export default function Optimizing(){
                     The point isn't to be perfectly cheerful all the time. The point is that happiness is a choice, and I want to give you a tool to make that choice a little bit easier!
                 </p>
             </div>
-            <img src='/images/Happy-person.jpg' alt="Image of a happy child" id='happyPerson'></img>
+            <img src='./images/Happy-person.jpg' alt="Image of a happy child" id='happyPerson'></img>
         </div>
     )
 }
